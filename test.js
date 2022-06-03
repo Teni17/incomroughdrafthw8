cd6b3c65a4c9ab3997ac2900fd5363e007cb5084
Teni17 <103621519+Teni17@users.noreply.github.com>
2022-06-03 15:18:51 -0700

    Add files via upload

--- test.js
+++ test.js
@@ -1,3 +1,79 @@
+function display(){
+var situpsArray = [
+[6, 12, 18, 24, 20],
+[8, 13, 18, 23, 28],
+[10, 14, 18, 22, 26],
+[12, 15, 18, 21, 24],
+[15, 17, 19, 21, 23],
+];
+
+var joggingArray = [
+[10, 16, 22, 28, 34],
+[12, 17, 22, 27, 32],
+[14, 18, 22, 26, 30],
+[16, 19, 22, 25, 28],
+[18, 20, 22, 24, 26],
+];
+
+var pushupsArray = [
+[4, 9, 14, 19, 24],
+[6, 11, 16, 19, 23],
+[8, 12, 16, 20, 24],
+[10, 13, 16, 19, 21],
+[12, 14, 16, 18, 20],
+
+
+];
+
+//person in weight Category 3 and height Cat 2 would be (these are the 2 values you set with your 10 if statements
+if(userObject.Weight<70 )
+{
+return(weightCat = 0)
+}
+if(userObject.Weight >=70 && userObject.Weight < 120 )
+{
+return(weightCat = 1)
+}
+if(userObject.Weight >=120 && userObject.Weight < 170 )
+{
+return(weightCat = 2)
+}
+if(userObject.Weight>=170 && userObject.Weight < 220 )
+{
+return(weightCat = 3)
+}
+if(userObject.Weight>=220 )
+{
+return(weightCat = 4)
+}
+if(userObject.Height <= 100)
+{
+return(heightCat = 0)
+}
+if(userObject.Height> 100 && userObject.Height < 130)
+{
+return(heightCat = 1)
+}
+if(userObject.Height>= 130 && userObject.Height < 160)
+{
+return(heightCat = 2)
+}
+if(userObject.Height>= 160 && userObject.Height < 190)
+{
+return(heightCat = 3)
+}
+if(userObject.Height>= 190 )
+{
+return(heightCat = 4)
+}
+
+let heightCat = 0;
+let weightCat = 4;
+console.log("You should do " + situpsArray[heightCat] [weightCat] + " situps");
+console.log("You should jog " + joggingArray[heightCat] [weightCat] + " minutes");
+console.log("You should do " + pushupsArray[heightCat] [weightCat] + " pushups");
+}
+
 let userArray = [];
 
 let userObject = function(pName,pHeight,pWeight){
@@ -25,13 +101,13 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     document.getElementById("buttonSortHeight").addEventListener("click", function () {
-        userArray.sort(userSort(Height));
+        userArray.sort(dynamicSort("Height"));
         createList();
         document.location.href = "test.html#ListAll";
     });
 
     document.getElementById("buttonSortWeight").addEventListener("click", function () {
-        userArray.sort(userSort(Weight));
+        userArray.sort(dynamicSort("Weight"));
         createList();
         document.location.href = "test.html#ListAll";
     });
@@ -79,11 +155,31 @@ function createList() {
     });
   
 };
-function userSort() {
-  
- (function(a,b){return a-b});
-    
+
+
+
+
+
+
+//* @param {String} property Key of the object to sort.
+
+function dynamicSort(property) {
+    var sortOrder = 1;
+
+    if (property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+
+    return function (a, b) {
+        if (sortOrder == -1) {
+            return b[property]-(a[property]);
+        } else {
+            return a[property]-(b[property]);
+        }
+    }
 }
+
 
 // function sportrecommend(){
 // if(height>170){
